bf269fe989a6ab2570e3a0353c54b29624fb4b39
Courtney Downs <courtneysdowns@gmail.com>
2021-08-11 20:58:35 -0400

    updating DatabaseOfScreams.html, SearchYourScream.html and script.js

--- script.js
+++ script.js
@@ -1,87 +1,96 @@
+const horrorSearchForm = document.querySelector(".form");
 const inpHorrorTitle = document.getElementById("inp-horror-title");
 const btnHorrorTitle = document.getElementById("btn-horror-title");
 const horrorTitleResults = document.getElementById("horror-title-results");
 btnHorrorTitle.addEventListener("click", getHorrorMoviesByTitle);
 
 const inpHorrorDatabase = document.getElementById("inp-horror-database");
-const btnHorrorDatabase = document.getElementById("btn-horror-databse");
+const btnHorrorDatabase = document.getElementById("btn-horror-database");
 const horrorDatabaseResults = document.getElementById(
   "horror-database-results"
 );
-btnHorrorDatabase.addEventListener("click", getHorrorMovieDatabase);
+btnHorrorDatabase.addEventListener("click", horrorMovieDatabase);
+const nextBtn = document.querySelector("#next-button");
+nextBtn.addEventListener("click", nextPage);
 
-function getHorrorMoviesByTitle() {
+function getHorrorMoviesByTitle(e) {
+  e.preventDefault();
   const searchWordsTitle = inpHorrorTitle.value.trim().replace(/\s/, "+");
-
   fetch(`http://www.omdbapi.com/?apikey=dde3f76d&t=${searchWordsTitle}`)
     .then((result) => result.json())
     .then((json) => displayFetch(json));
+}
 
-  function displayFetch(movies) {
-    genre = movies.Genre;
-    console.log(movies);
-    console.log(genre);
-    if (genre.includes("Horror")) {
-      let poster = document.createElement("img");
-      let title = document.createElement("h1");
-      let value = document.createElement("h4");
-      let year = document.createElement("p");
-      // let source = document.createElement("p");
-      let rated = document.createElement("p");
-      let runtime = document.createElement("p");
-      let director = document.createElement("p");
-      let actors = document.createElement("p");
-      let plot = document.createElement("p");
+function displayFetch(movies) {
+  while (horrorTitleResults.firstChild) {
+    horrorTitleResults.removeChild(horrorTitleResults.firstChild);
+  }
+  genre = movies.Genre;
+  console.log(movies);
+  console.log(genre);
+  if (genre.includes("Horror")) {
+    let poster = document.createElement("img");
+    let title = document.createElement("h1");
+    let value = document.createElement("h4");
+    let year = document.createElement("p");
+    // let source = document.createElement("p");
+    let rated = document.createElement("p");
+    let runtime = document.createElement("p");
+    let director = document.createElement("p");
+    let actors = document.createElement("p");
+    let plot = document.createElement("p");
 
-      poster.src = movies.Poster;
-      console.log(poster.src);
-      title.innerText = movies.Title;
-      value.innerText = movies.Ratings[0].Value;
-      year.innerText = movies.Year;
-      rated.innerText = movies.Rated;
-      runtime.innerText = movies.Runtime;
-      director.innerText = movies.Director;
-      actors.innerText = movies.Actors;
-      // title.href = `https://imdb.com/title/${imdbID}`;
-      // source.innerText = movies.Ratings[0].Value;
-      plot.innerText = movies.Plot;
+    poster.src = movies.Poster;
+    console.log(poster.src);
+    title.innerText = movies.Title;
+    value.innerText = movies.Ratings[0].Value;
+    year.innerText = movies.Year;
+    rated.innerText = movies.Rated;
+    runtime.innerText = movies.Runtime;
+    director.innerText = movies.Director;
+    actors.innerText = movies.Actors;
+    // title.href = `https://imdb.com/title/${imdbID}`;
+    // source.innerText = movies.Ratings[0].Value;
+    plot.innerText = movies.Plot;
 
-      horrorTitleResults.appendChild(poster);
-      horrorTitleResults.appendChild(title);
-      horrorTitleResults.appendChild(value);
-      horrorTitleResults.appendChild(year);
-      horrorTitleResults.appendChild(rated);
-      horrorTitleResults.appendChild(runtime);
-      horrorTitleResults.appendChild(director);
-      // horrorResults.appendChild(source);
-      horrorTitleResults.appendChild(actors);
-      horrorTitleResults.appendChild(plot);
-      horrorTitleResults.style.cursor = "pointer";
+    horrorTitleResults.appendChild(poster);
+    horrorTitleResults.appendChild(title);
+    horrorTitleResults.appendChild(value);
+    horrorTitleResults.appendChild(year);
+    horrorTitleResults.appendChild(rated);
+    horrorTitleResults.appendChild(runtime);
+    horrorTitleResults.appendChild(director);
+    // horrorResults.appendChild(source);
+    horrorTitleResults.appendChild(actors);
+    horrorTitleResults.appendChild(plot);
+    horrorTitleResults.style.cursor = "pointer";
 
-      // horrorResults.setAttribute("class", "card");
-      // card.setAttribute("class", "body")
-      // body.setAttribute("class", "title")
+    // horrorResults.setAttribute("class", "card");
+    // card.setAttribute("class", "body")
+    // body.setAttribute("class", "title")
 
-      poster.addEventListener("click", (movie) => {
-        fetch(
-          `https://api.themoviedb.org/3/movie/${movies.imdbID}?api_key=10290d885ea485489931ec4bd8364b33&language=en-US`
-        )
-          .then((result) => result.json())
-          .then((json) => {
-            console.log(json);
-            window.open(`https://www.imdb.com/title/${json.imdb_id}`);
-          });
-      });
-    } else {
-      alert("This is not a horror movie!");
-    }
+    poster.addEventListener("click", (movie) => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${movies.imdbID}?api_key=f6c2597101b3b0f8d6fcff87b23eaea3&language=en-US`
+      )
+        .then((result) => result.json())
+        .then((json) => {
+          console.log(json);
+          window.open(`https://www.imdb.com/title/${json.imdb_id}`);
+        });
+    });
+  } else {
+    alert("This is not a horror movie!");
   }
 }
 
-let currentHorrorMoviePage = 0;
+let currentHorrorMoviePage = 1;
+function nextPage(e) {
+  currentHorrorMoviePage++;
+  horrorMovieDatabase(e);
+}
 function horrorMovieDatabase() {
   // const searchWordsDatabase = inpHorrorTitle.value.trim().replace(/\s/, "+");
-  currentHorrorMoviePage = currentHorrorMoviePage + 1;
   fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=f6c2597101b3b0f8d6fcff87b23eaea3&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27&with_original_language=en&page=${currentHorrorMoviePage}`
   )
@@ -109,7 +118,7 @@ function horrorMovieDatabase() {
 
         movieItem.addEventListener("click", (movie) => {
           fetch(
-            `https://api.themoviedb.org/3/movie/${result.id}?api_key=10290d885ea485489931ec4bd8364b33&language=en-US`
+            `https://api.themoviedb.org/3/movie/${result.id}?api_key=f6c2597101b3b0f8d6fcff87b23eaea3&language=en-US`
           )
             .then((result) => result.json())
             .then((json) => {
@@ -118,13 +127,13 @@ function horrorMovieDatabase() {
             });
         });
         fetch(
-          `https://api.themoviedb.org/3/movie/${result.id}/images?api_key=10290d885ea485489931ec4bd8364b33&language=en-US`
+          `https://api.themoviedb.org/3/movie/${result.id}/images?api_key=f6c2597101b3b0f8d6fcff87b23eaea3&language=en-US`
         )
           .then((result) => result.json())
           .then((json) => {
             console.log(json);
           });
-        horrorDatabseResults.appendChild(movieItem);
+        horrorDatabaseResults.appendChild(movieItem);
       });
       // horrorResults.innerHTML = htmlString;
     });
